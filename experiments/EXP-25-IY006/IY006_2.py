872d7980d85aa1ee4ca99e1f133dd73f443af9bc
GrignardReagent <ian16yang@gmail.com>
2025-06-16 09:37:42 +0100

    completed IY006_2 hyperparam finetuning on the HPC, using CV 0.32 data.

--- experiments/EXP-25-IY006/IY006_2.py
+++ experiments/EXP-25-IY006/IY006_2.py
@@ -24,7 +24,7 @@ from utils.load_data import load_and_split_data
 # Data preparation (same dataset and preprocessing as IY006.py)
 # ---------------------------------------------------------------------------
 # USING CV FIXED DATASET
-DATA_FILE = "/exports/eddie/scratch/s1732775/SSA/experiments/EXP-25-IY001/data/combined_traj_cv_0.32_0.32_SS.csv"
+DATA_FILE = "/exports/eddie/scratch/s1732775/SSA/experiments/EXP-25-IY006/data/combined_traj_cv_0.32_0.32_SS.csv"
 
 # Load data with an explicit validation split
 X_train, X_val, X_test, y_train, y_val, y_test = load_and_split_data(
