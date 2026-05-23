ac6aa904cf81f34e01990925cb9bd5ce30679bb3
GrignardReagent <ian16yang@gmail.com>
2026-05-23 11:50:56 +0100

    IY029 added new compariosn method - lstm

---
+++ experiments/EXP-26-IY029/IY029_lstm_pairwise.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Grid Engine options
+#$ -N IY029_lstm_pairwise
+#$ -o IY029_lstm_pairwise.o$JOB_ID
+#$ -e IY029_lstm_pairwise.e$JOB_ID
+#$ -cwd
+#$ -l h_rt=47:59:59
+#$ -q gpu
+#$ -l gpu=1
+#$ -l h_rss=32G
+#$ -m bea -M s1732775@ed.ac.uk
+
+# Initialise environment modules
+. /etc/profile.d/modules.sh
+module load miniforge
+module load cuda
+
+# Activate env
+conda activate stochastic_sim
+
+# Run Python script and log output
+python IY029_lstm_pairwise.py > IY029_lstm_pairwise.out 2>&1
+
+# Deactivate after job is done
+conda deactivate
